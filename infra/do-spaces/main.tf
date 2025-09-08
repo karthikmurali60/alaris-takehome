6b7ac71060591e63c31244bbb8043348c68728f1
Karthik Muraliprasad <karthik.muraliprasad@gmail.com>
2025-09-08 13:41:19 -0500

    Added workflow for DO Spaces provisioning (#2)

---
+++ infra/do-spaces/main.tf
@@ -0,0 +1,18 @@
+resource "digitalocean_spaces_bucket" "backups" {
+  name   = var.space_name
+  region = var.region
+
+  acl    = "private"
+}
+
+resource "digitalocean_spaces_bucket_cors_configuration" "example" {
+  bucket = digitalocean_spaces_bucket.backups.name
+  region = "nyc1"
+
+  cors_rule {
+    allowed_methods = ["GET", "PUT", "POST", "HEAD", "DELETE"]
+    allowed_origins = ["*"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
+}
---
+++ infra/do-spaces/task/terraform.tfvars
@@ -0,0 +1,2 @@
+space_name=alaris-takehome-karthikmuraliprasad
+region=nyc1
