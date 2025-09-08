5aacbcd03d1b0474f80baeb6c645f04604592a46
karthikmurali60 <karthik.murali60@gmail.com>
2025-09-08 17:02:10 -0500

    Updated remote backend for TfState

--- infra/do-spaces/providers.tf
+++ infra/do-spaces/providers.tf
@@ -5,7 +5,24 @@ terraform {
       version = "~> 2.66.0"
     }
   }
-  required_version = ">= 1.4.0"
+
+  required_version = ">= 1.6.3"
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://nyc3.digitaloceanspaces.com"
+    }
+
+    bucket = "alaris-takehome-task-tfstate"
+    key    = "task/terraform.tfstate"
+
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_s3_checksum            = true
+    region                      = "us-east-1"
+  }
 }
 
 provider "digitalocean" {
